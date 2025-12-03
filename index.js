bacbcd725db5bb8500db9f12d2563328fd5918fb
EpicuriousDev <Luukvanoosterhout604@gmail.com>
2025-12-03 20:26:24 +0100

    Final ed. pc only.
    hierna komt nog mobiel en laptop

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-        const arrow = entry.target.querySelector('.scroll-arrow');
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      const arrow = entry.target.querySelector(".scroll-arrow");
 
-        if (!arrow) return;
+      if (!arrow) return;
 
-        if (entry.isIntersecting) {
-            arrow.classList.add('visible');
-        } else {
-            arrow.classList.remove('visible');
-        }
+      if (entry.isIntersecting) {
+        arrow.classList.add("visible");
+      } else {
+        arrow.classList.remove("visible");
+      }
     });
-}, {
-    threshold: 1
-});
+  },
+  {
+    threshold: 1,
+  }
+);
 
-
-document.querySelectorAll('.box-centraal').forEach(section => {
-    observer.observe(section);
+document.querySelectorAll(".box-centraal").forEach((section) => {
+  observer.observe(section);
 });
